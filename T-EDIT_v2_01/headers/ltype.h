c7b3c9c9a8aab0f02d728179e3b07e44890fe215
tatacharles <tatacharles@gmail.com>
2014-08-19 15:07:52 +0100

    headers
    headers

---
+++ T-EDIT_v2_01/headers/ltype.h
@@ -0,0 +1,19 @@
+#ifndef LTYPE_H_INCLUDED
+#define LTYPE_H_INCLUDED
+
+typedef struct vec3f vec3f;
+struct vec3f
+{
+    float x;
+    float y;
+    float z;
+};
+
+typedef struct vec2f vec2f;
+struct vec2f
+{
+    float x;
+    float y;
+};
+
+#endif // LTYPE_H_INCLUDE
